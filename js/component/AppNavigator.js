9667ba41665a1068d4157dbc293faba5526ade27
lixiaowei <lixiaowei@91yong.com>
2016-10-13 16:54:14 +0800

    关闭navigator的侧滑手势

--- js/component/AppNavigator.js
+++ js/component/AppNavigator.js
@@ -1,28 +1,31 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import {
-	Navigator,
+    Navigator,
 } from 'react-native';
 //应用导航器
-export default class AppNavigator extends Component{
-	constructor(props){
-		super(props);
-	}
-	render(){
-		return(
-			<Navigator 
-		        initialRoute={{id:this.props.id,data:this.props.data,name:this.props.name,component:this.props.component}}
-		        renderScene={(route,navigator)=>{
-		          let Scene = route.component;
-		          return <Scene id={route.id} data={route.data} name={route.name} navigator={navigator}/>
-		        }}
-		        style={{flex:1,}}
-		        configureScene={(route) => {
-		          if(route.sceneConfig){
-		            return route.sceneConfig;
-		          }
-		          return Navigator.SceneConfigs.HorizontalSwipeJump;
-		        }}
-		      />
-			);
-	}
+export default class AppNavigator extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{id: this.props.id, data: this.props.data, name: this.props.name, component: this.props.component}}
+                renderScene={(route, navigator)=> {
+                    let Scene = route.component;
+                    return <Scene id={route.id} data={route.data} name={route.name} navigator={navigator}/>
+                }}
+                style={{flex: 1,}}
+                configureScene={(route) => {
+                    if (route.sceneConfig) {
+                        return route.sceneConfig;
+                    }
+                    var conf = Navigator.SceneConfigs.HorizontalSwipeJump;
+                    conf.gestures = null;
+                    return conf;
+                }}
+            />
+        );
+    }
 }
