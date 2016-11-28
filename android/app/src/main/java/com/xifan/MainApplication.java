cb240cbd4a41c2c4c201baece51b209b94c4b228
lixiaowei <lixiaowei@91yong.com>
2016-11-28 18:32:15 +0800

    更新react@15.4.1/react-native@0.38.0

--- android/app/src/main/java/com/xifan/MainApplication.java
+++ android/app/src/main/java/com/xifan/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 import com.xifan.module.OrientationPackage;
 import com.xifan.module.VideoViewPackage;
 
@@ -16,25 +17,31 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        protected boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
-                    new MainReactPackage(),
-                    new OrientationPackage(),
-                    new VideoViewPackage(),
-                    new SQLitePluginPackage()
-            );
-        }
-    };
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
     @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+              new MainReactPackage(),
+              new OrientationPackage(),
+              new VideoViewPackage(),
+              new SQLitePluginPackage()
+      );
     }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
